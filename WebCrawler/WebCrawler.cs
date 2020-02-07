471dc7f40dd892b701fda0ddebf9bde1180fa12f
Max <max.pewny@gmail.com>
2020-02-07 14:19:12 +0100

    Multithreadding added

--- WebCrawler/WebCrawler.cs
+++ WebCrawler/WebCrawler.cs
@@ -1,38 +1,73 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
 using System.Text.RegularExpressions;
 
 namespace WebCrawler
 {
     class WebCrawler
     {
-        HashSet<string> mReadLinks = new HashSet<string>();
+        static HashSet<string> mReadLinks = new HashSet<string>();
+        static object mutex = new object();
 
         public void FindExpressions(string pUrl, string pSearchedExpression, int pSearchDepth)
         {
-            int expressionsCount = 0;
-            string content;
-            if (pSearchDepth > 0 && !mReadLinks.Contains(pUrl))
+            bool containsUrl;
+
+            lock (mutex)
             {
-                Console.WriteLine("NEW WEBSITE: {0}", pUrl);
-                mReadLinks.Add(pUrl);
-                using (var wc = new System.Net.WebClient())
+                containsUrl = mReadLinks.Contains(pUrl);
+            }
+
+            if (pSearchDepth > 0 && !containsUrl)
+            {
+                int expressionsCount = 0;
+                string content;
+                List<Thread> threads = new List<Thread>();
+
+                //Console.WriteLine("NEW WEBSITE: {0}", pUrl);
+        
+                lock (mutex)
                 {
-                    content = wc.DownloadString(pUrl);
+                    mReadLinks.Add(pUrl);
                 }
+                try
+                {
+                    using (var wc = new System.Net.WebClient())
+                    {
+                        content = wc.DownloadString(pUrl);
+                    }
+                }
+                catch (System.Net.WebException e) 
+                {
+                    Console.WriteLine("EXCEPTION AT {0} : {1}", pUrl, e.Message);
+                    return;
+                }
+   
+
 
                 foreach (Match match in Regex.Matches(content, pSearchedExpression))
                 {
                     ++expressionsCount;
                 }
-                Console.WriteLine("EXPRESSION '{0}' FOUND: {1}", pSearchedExpression, expressionsCount);
+                Console.WriteLine("EXPRESSION '{0}' FOUND IN {1} : {2}", pSearchedExpression, pUrl, expressionsCount);
 
                 foreach (Match match in Regex.Matches(content, "href=\"(https?://www\\.games-academy\\.de)?/([^\"#\\?:.]*)[\"#\\?]"))
                 {
                     var link = "http://www.games-academy.de/" + match.Groups[2].Value;
                     //Console.WriteLine("LINK: {0}", link);
-                    FindExpressions(link, pSearchedExpression, pSearchDepth - 1);
+                    Thread thread = new System.Threading.Thread(() =>
+                        {
+                            FindExpressions(link, pSearchedExpression, pSearchDepth - 1);
+                        });
+                    threads.Add(thread);
+                    thread.Start();
+                }
+
+                foreach (var thread in threads)
+                {
+                    thread.Join();
                 }
 
                 
