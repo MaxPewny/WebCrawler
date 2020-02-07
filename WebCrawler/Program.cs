d455aecf2840a7078cef97abcaf074e6f39fc7bc
Max <max.pewny@gmail.com>
2020-02-07 12:11:24 +0100

    Created Crawler

---
+++ WebCrawler/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace WebCrawler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WebCrawler webCrawler = new WebCrawler();
+
+            webCrawler.FindExpressions("http://www.games-academy.de/", "Game", 3);
+        }
+    }
+}
---
+++ WebCrawler/WebCrawler.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace WebCrawler
+{
+    class WebCrawler
+    {
+        HashSet<string> mReadLinks = new HashSet<string>();
+
+        public void FindExpressions(string pUrl, string pSearchedExpression, int pSearchDepth)
+        {
+            int expressionsCount = 0;
+            string content;
+            if (pSearchDepth > 0 && !mReadLinks.Contains(pUrl))
+            {
+                Console.WriteLine("NEW WEBSITE: {0}", pUrl);
+                mReadLinks.Add(pUrl);
+                using (var wc = new System.Net.WebClient())
+                {
+                    content = wc.DownloadString(pUrl);
+                }
+
+                foreach (Match match in Regex.Matches(content, pSearchedExpression))
+                {
+                    ++expressionsCount;
+                }
+                Console.WriteLine("EXPRESSION '{0}' FOUND: {1}", pSearchedExpression, expressionsCount);
+
+                foreach (Match match in Regex.Matches(content, "href=\"(https?://www\\.games-academy\\.de)?/([^\"#\\?:.]*)[\"#\\?]"))
+                {
+                    var link = "http://www.games-academy.de/" + match.Groups[2].Value;
+                    //Console.WriteLine("LINK: {0}", link);
+                    FindExpressions(link, pSearchedExpression, pSearchDepth - 1);
+                }
+
+                
+            }
+            
+        }
+
+
+        
+    }
+}
